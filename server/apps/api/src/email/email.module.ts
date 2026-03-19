de21f3f84b6b65dc90f099bf21846ad0353f4ffb
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2026-03-19 12:22:04 +0100

    feat: introduce global email module with email service, config, and prisma integration.

--- server/apps/api/src/email/email.module.ts
+++ server/apps/api/src/email/email.module.ts
@@ -1,10 +1,11 @@
 import { Module, Global } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { EmailService } from './email.service'
+import { PrismaModule } from '../prisma/prisma.module'
 
 @Global()
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, PrismaModule],
   providers: [EmailService],
   exports: [EmailService],
 })
