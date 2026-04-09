e7a980c991dbc2dad7a5e67b6347961d601182c3
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2026-04-09 16:19:50 +0100

    fixed liases 5

--- server/apps/api/src/application/application.module.ts
+++ server/apps/api/src/application/application.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common'
 import { AdminAuditEventHandler } from './events/handlers/admin-audit.handler'
 import { EmailLogEventHandler } from './events/handlers/email-log.handler'
 import { InteractionHandler } from './events/handlers/interaction.handler'
-import { S3Module } from '@shared/infrastructure/common/s3/s3.module'
-import { ReceiptModule } from '@shared/infrastructure/common/receipt/receipt.module'
-import { WebhookService } from '@shared/infrastructure/common/webhook/webhook.service'
+import { S3Module } from '../shared/infrastructure/common/s3/s3.module'
+import { ReceiptModule } from '../shared/infrastructure/common/receipt/receipt.module'
+import { WebhookService } from '../shared/infrastructure/common/webhook/webhook.service'
 
 // Use Cases
 import { DeleteAdminUseCase } from './use-cases/admin/delete-admin.use-case'
