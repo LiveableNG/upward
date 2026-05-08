300cfacd0b37cac3794c1b0995eed05d3e25c658
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2026-05-08 22:56:43 +0100

    fixed saving template issus

--- server/apps/api/src/interfaces/http/controllers/pm-document.controller.ts
+++ server/apps/api/src/interfaces/http/controllers/pm-document.controller.ts
@@ -1,9 +1,9 @@
-
-import { Controller, Get, Post, Patch, Body, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Body, UseGuards, Request, Inject, UnauthorizedException } from '@nestjs/common';
 import { JwtAuthGuard } from '../../../application/auth/guards/jwt-auth.guard';
 import { GetPmDocumentsUseCase } from '../../../application/pm/use-cases/documents/get-pm-documents.use-case';
 import { SaveDocumentTemplateUseCase, SaveDocumentTemplateDto } from '../../../application/pm/use-cases/documents/save-document-template.use-case';
 import { SendDocumentUseCase, SendDocumentDto } from '../../../application/pm/use-cases/documents/send-document.use-case';
+import { PropertyManagerRepository, PROPERTY_MANAGER_REPOSITORY } from '../../../domains/pm/property-manager.repository';
 
 @Controller('pm/documents')
 @UseGuards(JwtAuthGuard)
@@ -12,20 +12,32 @@ export class PmDocumentController {
     private readonly getDocumentsUseCase: GetPmDocumentsUseCase,
     private readonly saveTemplateUseCase: SaveDocumentTemplateUseCase,
     private readonly sendDocumentUseCase: SendDocumentUseCase,
+    @Inject(PROPERTY_MANAGER_REPOSITORY) private readonly pmRepository: PropertyManagerRepository,
   ) {}
+
+  private async getPmId(req: any): Promise<number> {
+    const uuid = req.user?.sub;
+    if (!uuid) throw new UnauthorizedException('Invalid user context');
+    const pm = await this.pmRepository.findByUuid(uuid);
+    if (!pm || !pm.id) throw new UnauthorizedException('Property Manager not found');
+    return pm.id;
+  }
 
   @Get()
   async getDocuments(@Request() req: any) {
-    return this.getDocumentsUseCase.execute(req.user.id);
+    const pmId = await this.getPmId(req);
+    return this.getDocumentsUseCase.execute(pmId);
   }
 
   @Post('templates')
   async saveTemplate(@Request() req: any, @Body() data: SaveDocumentTemplateDto) {
-    return this.saveTemplateUseCase.execute(req.user.id, data);
+    const pmId = await this.getPmId(req);
+    return this.saveTemplateUseCase.execute(pmId, data);
   }
 
   @Post('send')
   async sendDocument(@Request() req: any, @Body() data: SendDocumentDto) {
-    return this.sendDocumentUseCase.execute(req.user.id, data);
+    const pmId = await this.getPmId(req);
+    return this.sendDocumentUseCase.execute(pmId, data);
   }
 }
