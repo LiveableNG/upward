c093ae8b092ff96f725df29bc5e6622f66c7ef72
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2026-05-10 20:11:02 +0100

    feat: initialize API workspace and add landlord password change use case

--- server/apps/api/src/application/pm/use-cases/landlord/landlord-change-password.use-case.ts
+++ server/apps/api/src/application/pm/use-cases/landlord/landlord-change-password.use-case.ts
@@ -1,5 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { LandlordAuthService } from '../../auth/landlord-auth.service';
+import { LandlordAuthService } from '../../../auth/landlord-auth.service';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
