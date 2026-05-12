0dcdb2ffb302b33a13eb20e049e657e629f1f15f
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2026-05-12 09:29:00 +0100

    handled replaced logic

--- server/apps/api/src/application/use-cases/pm/update-pm-profile.use-case.ts
+++ server/apps/api/src/application/use-cases/pm/update-pm-profile.use-case.ts
@@ -25,6 +25,18 @@ export class UpdatePmProfileUseCase {
     const pm = await this.pmRepository.findByUuid(pmUuid)
     if (!pm) throw new NotFoundException('Property manager not found')
 
+    // 1. Handle file deletions for replaced/removed items
+    if (dto.profilePic !== undefined && pm.profilePic && pm.profilePic !== dto.profilePic) {
+      await this.s3Service.deleteObject(pm.profilePic)
+    }
+    if (dto.letterheadHeaderUrl !== undefined && pm.letterheadHeaderUrl && pm.letterheadHeaderUrl !== dto.letterheadHeaderUrl) {
+      await this.s3Service.deleteObject(pm.letterheadHeaderUrl)
+    }
+    if (dto.letterheadFooterUrl !== undefined && pm.letterheadFooterUrl && pm.letterheadFooterUrl !== dto.letterheadFooterUrl) {
+      await this.s3Service.deleteObject(pm.letterheadFooterUrl)
+    }
+
+    // 2. Update the profile
     const updatedPm = await this.pmRepository.update(pm.id!, dto)
     
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -35,8 +47,15 @@ export class UpdatePmProfileUseCase {
       ...rest
     }
 
+    // 3. Sign all S3 URLs for the client
     if (clientProfile.profilePic) {
       clientProfile.profilePic = await this.s3Service.getDownloadUrl(clientProfile.profilePic)
+    }
+    if (clientProfile.letterheadHeaderUrl) {
+      clientProfile.letterheadHeaderUrl = await this.s3Service.getDownloadUrl(clientProfile.letterheadHeaderUrl)
+    }
+    if (clientProfile.letterheadFooterUrl) {
+      clientProfile.letterheadFooterUrl = await this.s3Service.getDownloadUrl(clientProfile.letterheadFooterUrl)
     }
 
     return clientProfile
