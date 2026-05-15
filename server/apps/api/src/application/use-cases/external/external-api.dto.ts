c7876a2d276a17d8b4cd556c78830b33a76410e1
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2026-05-15 05:26:48 +0100

    fixed payment request

--- server/apps/api/src/application/use-cases/external/external-api.dto.ts
+++ server/apps/api/src/application/use-cases/external/external-api.dto.ts
@@ -269,6 +269,10 @@ export class ExternalPaymentRequestPayloadDto {
   @IsOptional()
   @IsString()
   rentType?: string
+
+  @IsOptional()
+  @IsNumber()
+  paymentRequestId?: number
 }
 
 export class AddPropertyPayloadDto {
